5746bcc65df2f715d9e4defc1cb2484889f12c34
BT_1N3_07@BKTECH.LOCAL <BT_1N3_07@BKTECH.LOCAL>
2018-09-17 10:00:41 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        public static boolean dateStr(String date) {
+            return date.substring(0,2) + "-" + date.substring(3,5) + "-" + date.substring(6);
+        }
+    }
+}
